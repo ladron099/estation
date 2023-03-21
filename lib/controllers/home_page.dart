40ec025a11d58854861de32cbbcbed9efff62bc0
VTapp1 <covidtrackerapp97@gmail.com>
2023-03-21 17:36:20 +0000

    f

---
+++ lib/controllers/home_page.dart
@@ -0,0 +1,6 @@
+import 'package:estation/screens/home_page.dart';
+import 'package:get/get.dart';
+
+class HomePageController extends GetxController {
+  String test = "yasser";
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,16 +1,23 @@
+import 'package:estation/controllers/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
 
 class HomePageScreen extends StatelessWidget {
   const HomePageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
-      ),
+    return GetBuilder<HomePageController>(
+      init: HomePageController(),
+      builder: (controller) {
+        return Scaffold(
+          body: Center(
+            child: Text('Home Page ${controller.test}'),
+          ),
+        );
+      }
     );
   }
 }
